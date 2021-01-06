78c6ab8eeb256e9f1ad390fc38865f36ce38d3ff
Aidan Murray <aidanhansm@gmail.com>
2021-01-06 15:40:02 -0700

    Start oauth stuff
    Co-authored-by: Will Dunlap <dunlapww@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
-  # If you're using a strategy that POSTs during callback, you'll need to skip the authenticity token check for the callback action only. 
+  # If you're using a strategy that POSTs during callback, you'll need to skip the authenticity token check for the callback action only.
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     self.current_user = @user
+    require 'pry'; binding.pry
     redirect_to '/'
   end
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'] #, redirect_uri: "https://localhost:3000/auth/google/callback"
 end
