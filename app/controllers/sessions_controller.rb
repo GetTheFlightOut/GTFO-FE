c863fb85acdf68eed9a5d73bd3d4d131a2bd4619
Will Dunlap <dunlapww@gmail.com>
2021-01-06 16:27:26 -0700

    remove pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     self.current_user = @user
-    require 'pry'; binding.pry
     redirect_to '/'
   end
 
