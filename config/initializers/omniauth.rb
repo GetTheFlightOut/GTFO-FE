ab3853753cc803e6f11bce2f7820bb2aa8cadb21
Will Dunlap <dunlapww@gmail.com>
2021-01-06 15:55:22 -0700

    bad stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'] #, redirect_uri: "https://localhost:3000/auth/google/callback"
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/google', as: :google_login
+  get '/auth/google_oauth2', as: :google_login
 
   get '/', to: 'welcome#index', as: :root
 end
