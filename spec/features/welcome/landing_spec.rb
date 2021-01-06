2795e09db9942b7150d48409a8bca8f9f058c601
Aidan Murray <aidanhansm@gmail.com>
2021-01-06 13:54:24 -0700

    Add welcome page unauthorized visible elements test
    Co-authored-by: Will Dunlap <dunlapww@users.noreply.github.com>

--- spec/features/welcome/landing_spec.rb
+++ spec/features/welcome/landing_spec.rb
@@ -1,3 +1,19 @@
 require 'rails_helper'
 
-describe ''
+describe 'welcome page' do
+  describe 'As an unauthenticated user when I arrive at the website' do
+    it 'I see a page with a city, date, and duration dropdown menu and lucky location button' do
+      visit '/'
+
+      expect(page).to have_field('departure-airport')
+
+      expect(page).to have_field('departure-date')
+
+      expect(page).to have_field('trip-duration')
+
+      expect(page).to have_button('Lucky Location')
+
+      expect(page).to have_link('Log In with Google')
+    end
+  end
+end
