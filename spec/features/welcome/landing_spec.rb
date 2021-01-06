3263e0c2a2fadbe7ad5a24a5e478674161009feb
Will Dunlap <dunlapww@gmail.com>
2021-01-06 16:27:43 -0700

    Add test for oauth2.0
    Co-authored-by: Aidan Murray <aidanhansm@gmail.com>

--- spec/features/welcome/landing_spec.rb
+++ spec/features/welcome/landing_spec.rb
@@ -16,4 +16,45 @@ describe 'welcome page' do
       expect(page).to have_link('Log In with Google')
     end
   end
+
+  scenario 'user logs in using Google OAuth' do
+    # first, set OmniAuth to run in test mode
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+
+                                                                })
+
+    stub_omniauth
+    visit root_path
+    expect(page).to_not have_button('Search Locations')
+    expect(page).to have_link('Log In with Google')
+    click_link 'Log In with Google'
+    expect(page).to have_button('Search Locations')
+  end
+
+  def stub_omniauth
+    # first, set OmniAuth to run in test mode
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                         provider: 'google_oauth2',
+                                                                         uid: '12345678910',
+                                                                         info: { 'name' => 'William Dunlap',
+                                                                                 'email' => 'dunlapww@gmail.com',
+                                                                                 'unverified_email' => 'dunlapww@gmail.com',
+                                                                                 'email_verified' => true,
+                                                                                 'first_name' => 'William',
+                                                                                 'last_name' => 'Dunlap',
+                                                                                 'image' => 'https://lh3.googleusercontent.com/a-/AOh14GjhAh3U9dMZzp_IOL6z6pQZqIOSD2kPAtAPVLvrqg=s96-c' },
+                                                                         credentials: {
+                                                                           token: 'abcdefg12345',
+                                                                           expires_at: DateTime.now,
+                                                                           expires: true
+                                                                         }
+
+                                                                       })
+  end
 end
