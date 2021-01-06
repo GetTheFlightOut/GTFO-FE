cc1390e2b3c0144f57dd5b04a39f47559d2fb5ea
Will Dunlap <dunlapww@gmail.com>
2021-01-06 16:39:44 -0700

    Add shoulda-matchers to user model
    Co-authored-by: Aidan Murray <aidanhansm@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe User, type: :model do
   describe 'validations' do
-    
+    it {should validate_presence_of :uid}
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :first_name}
+    it {should validate_presence_of :last_name}
   end
 end
