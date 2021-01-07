36ace69f1e062298e1c46e4d298a742ac78b133f
Will Dunlap <dunlapww@gmail.com>
2021-01-07 09:55:00 -0700

    Add validations on user model
    Co-authored-by: Aidan Murray <aidanhansm@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  
+  validates :email, presence: true
+  validates :uid, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
