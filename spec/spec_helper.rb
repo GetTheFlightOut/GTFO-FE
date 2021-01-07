9ca0c001b4537a690ad1c370a614722be1d1ebd0
Will Dunlap <dunlapww@gmail.com>
2021-01-06 17:20:29 -0700

    Add test for Oauth redirect to welcome page
    Co-authored-by: Aidan Murray <aidanhansm@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,12 +95,5 @@ RSpec.configure do |config|
 =end
 end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
 
-    # Keep as many of these lines as are necessary:
-    with.library :active_record
-    with.library :active_model
-  end
-end
+
