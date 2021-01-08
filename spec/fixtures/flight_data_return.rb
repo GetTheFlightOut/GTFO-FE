4c7059e451cb2a39b991e0e445b40d6d5091cdb2
Connor Ferguson <cpfergus1@gmail.com>
2021-01-07 17:12:02 -0700

    Started creating fixtures for flight information test
    Co-authored-by: Aidan Murray <aidanhansm@gmail.com>

---
+++ spec/fixtures/flight_data_return.rb
@@ -0,0 +1,25 @@
+{
+  data: [
+      {
+        id: 123467,
+        datetime_departure: "2021-01-07T16:45:10-07:00",
+        datetime_arrival: "2021-01-07T16:47:12-07:00",
+        departure_loc: {
+          city: "Denver, CO"
+          airport: "DEN",
+          # lat: 39.8616667,
+          # long: -104.67306
+        }
+        arrival_loc:{
+          city: "Las Vegas, NV"
+          airport: "LAS",
+          # lat: 36.08,
+          # long: -115.15222
+        }
+        airlines: {
+          to: "frontier",
+          back: "frontier"
+        }
+      }
+    ]
+}
---
+++ spec/service/flight_information_service_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'flight service' do
+  it 'will return flight data' do
+
+  end
+end
