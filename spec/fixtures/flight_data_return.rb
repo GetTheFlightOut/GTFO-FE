e4f9060f9e44ab28b948987c29c7fbae1e40c92f
Connor Ferguson <cpfergus1@gmail.com>
2021-01-07 20:07:06 -0700

    Setup Fixtures file for testing
    Co-authored-by: Aidan Murray <aidanhansm@gmail.com>

--- spec/fixtures/flight_data_return.rb
+++ spec/fixtures/flight_data_return.rb
@@ -1,25 +1,128 @@
 {
   data: [
-      {
-        id: 123467,
-        datetime_departure: "2021-01-07T16:45:10-07:00",
-        datetime_arrival: "2021-01-07T16:47:12-07:00",
-        departure_loc: {
-          city: "Denver, CO"
-          airport: "DEN",
-          # lat: 39.8616667,
-          # long: -104.67306
-        }
-        arrival_loc:{
-          city: "Las Vegas, NV"
-          airport: "LAS",
-          # lat: 36.08,
-          # long: -115.15222
-        }
-        airlines: {
-          to: "frontier",
-          back: "frontier"
-        }
+    {
+      id: 123467,
+      datetime_departure: "2021-01-07T16:45:10-07:00",
+      datetime_arrival: "2021-01-07T16:47:12-07:00",
+      departure_loc: {
+        city: "Denver, CO"
+        airport: "DEN",
+        # lat: 39.8616667,
+        # long: -104.67306
+      },
+      arrival_loc:{
+        city: "Las Vegas, NV"
+        airport: "LAS",
+        # lat: 36.08,
+        # long: -115.15222
+      },
+      airlines: {
+        to: "frontier",
+        back: "frontier"
+      },
+      price: 48,
+      seat_availability: 7,
+      duration: "2h 1m",
+      average_forecast: {
+        is_available: true,
+        average_temp: 65,
+        chance_precip: 20
+        sky_coverage: 20
+        qualitative_forecast: "sunny"
       }
-    ]
+    },
+    {
+      id: 12345678,
+      datetime_departure: "2021-01-07T16:45:10-07:00",
+      datetime_arrival: "2021-01-07T16:47:12-07:00",
+      departure_loc: {
+        city: "Denver, CO"
+        airport: "DEN",
+        # lat: 39.8616667,
+        # long: -104.67306
+      },
+      arrival_loc:{
+        city: "Los Angeles, CA"
+        airport: "LAX",
+        # lat: 36.08,
+        # long: -115.15222
+      },
+      airlines: {
+        to: "frontier",
+        back: "frontier"
+      },
+      price: 58,
+      seat_availability: 7,
+      duration: "2h 1m",
+      average_forecast: {
+        is_available: true,
+        average_temp: 65,
+        chance_precip: 20
+        sky_coverage: 20
+        qualitative_forecast: "sunny"
+      }
+    },
+    {
+      id: 2345678,
+      datetime_departure: "2021-01-07T16:45:10-07:00",
+      datetime_arrival: "2021-01-07T16:47:12-07:00",
+      departure_loc: {
+        city: "Denver, CO"
+        airport: "DEN",
+        # lat: 39.8616667,
+        # long: -104.67306
+      },
+      arrival_loc:{
+        city: "Austin, TX"
+        airport: "AUS",
+        # lat: 36.08,
+        # long: -115.15222
+      },
+      airlines: {
+        to: "frontier",
+        back: "frontier"
+      },
+      price: 68,
+      seat_availability: 7,
+      duration: "2h 1m",
+      average_forecast: {
+        is_available: true,
+        average_temp: 65,
+        chance_precip: 20
+        sky_coverage: 20
+        qualitative_forecast: "sunny"
+      }
+    },
+    {
+      id: 3456789,
+      datetime_departure: "2021-01-07T16:45:10-07:00",
+      datetime_arrival: "2021-01-07T16:47:12-07:00",
+      departure_loc: {
+        city: "Denver, CO"
+        airport: "DEN",
+        # lat: 39.8616667,
+        # long: -104.67306
+      },
+      arrival_loc:{
+        city: "Miami, FL"
+        airport: "MIA",
+        # lat: 36.08,
+        # long: -115.15222
+      },
+      airlines: {
+        to: "frontier",
+        back: "frontier"
+      },
+      price: 78,
+      seat_availability: 7,
+      duration: "2h 1m",
+      average_forecast: {
+        is_available: true,
+        average_temp: 65,
+        chance_precip: 20
+        sky_coverage: 20
+        qualitative_forecast: "sunny"
+      }
+    }
+  ]
 }
--- spec/service/flight_information_service_spec.rb
+++ spec/service/flight_information_service_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe 'flight service' do
   it 'will return flight data' do
-
+    stub_request(:get, ENV[])
   end
 end
