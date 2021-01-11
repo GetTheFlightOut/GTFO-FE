e0b14d0f2feb7888dbf660db458931b072ac710c
todd-estes <elestes@gmail.com>
2021-01-10 17:20:27 -0700

    Update Trip poro with addtional attributes and date covert method
    Co-authored-by: Brian Liu 
    <67594471+badgerbreezy@users.noreply.github.com>

--- app/poros/trip.rb
+++ app/poros/trip.rb
@@ -4,6 +4,8 @@ class Trip
              :arrival_datetime,
              :r_departure_datetime,
              :r_arrival_datetime,
+             :r_departure_date,
+             :arrival_date,
              :origin_city,
              :destination_city,
              :price,
@@ -21,8 +23,10 @@ class Trip
     @flight_id = info[:attributes][:id].to_i
     @departure_datetime = info[:attributes][:departure_datetime]
     @arrival_datetime = info[:attributes][:arrival_datetime]
+    @arrival_date = date_convert(@arrival_datetime)
     @r_departure_datetime = info[:attributes][:r_departure_datetime]
     @r_arrival_datetime = info[:attributes][:r_arrival_datetime]
+    @r_departure_date = date_convert(@r_departure_datetime)
     @origin_city = info[:attributes][:origin_city]
     @destination_city = info[:attributes][:destination_city]
     @price = info[:attributes][:price]
@@ -35,5 +39,10 @@ class Trip
     @day_feels_like_f = info[:attributes][:day_feels_like_f]
     @booking_link = info[:attributes][:booking_link]
     @description = info[:attributes][:description]
+  end
+
+  def date_convert(date_time)
+    converted_time = DateTime.strptime(date_time, '%F')
+    converted_time.strftime('%F')
   end
 end
