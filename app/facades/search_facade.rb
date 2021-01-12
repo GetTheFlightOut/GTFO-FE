c21353de863a034f1c24968a58329d64e90dc1d9
Aidan Murray <aidanhansm@gmail.com>
2021-01-11 21:32:46 -0700

    Fix departure date formatting for api call
    Co-authored-by: George Soderholm <62966396+GeorgieGirl24@users.noreply.github.com>
    Co-authored-by: Brian Liu <67594471+badgerbreezy@users.noreply.github.com>
    Co-authored-by: Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,13 +1,16 @@
+require 'Date'
+
 class SearchFacade
   def self.get_lucky(params)
     params[:limit] = 1
-    flight_data = BackendService.trips_search(params)
+    flight_data = BackendService.trips_search(update_params(params))
+    # require 'pry'; binding.pry
     Trip.new(flight_data[:data][0])
   end
 
   def self.get_flights(params)
     params[:limit] = 20
-    flight_data = BackendService.trips_search(params)
+    flight_data = BackendService.trips_search(update_params(params))
     if flight_data[:data]
       flight_data[:data].map do |data|
         Trip.new(data)
@@ -16,5 +19,15 @@ class SearchFacade
     elsif flight_data[:error]
       flight_data[:error]
     end
+  end
+
+  private
+
+  def self.update_params(params)
+    flight_date = Date.new(*(params[:departure_date].split('-').map(&:to_i))).strftime("%m/%d/%Y")
+
+    params[:departure_date] = flight_date
+
+    params
   end
 end
