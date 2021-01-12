a63d1b8807bb6d0454639e8b4e61e9f9038e5125
Aidan Murray <aidanhansm@gmail.com>
2021-01-11 21:33:29 -0700

    Enable caching in development
    Co-authored-by: George Soderholm <62966396+GeorgieGirl24@users.noreply.github.com>
    Co-authored-by: Brian Liu <67594471+badgerbreezy@users.noreply.github.com>
    Co-authored-by: Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot.
   config.eager_load = true
@@ -22,7 +22,7 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{15.minutes.to_i}"
     }
   else
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
