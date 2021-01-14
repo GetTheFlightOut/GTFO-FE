c0a028f8db353460e7c6dd8267f4bd0bf77a4710
Nick King <nickmaxking@gmail.com>
2021-01-13 20:45:59 -0700

    Add test and functionality for search locations no results sad path

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,9 +6,13 @@ class FlightsController < ApplicationController
   def index
     if params[:commit] == 'Search Locations'
       @trips = SearchFacade.get_flights(flight_params)
-      return error_flash(@trips) if @trips.class == String
-      write_to_cache(@trips)
-      @hot_trips, @warm_trips, @cool_trips, @cold_trips = Trip.group_by_weather(@trips)
+      if @trips == []
+        redirect_to root_path, notice: 'There were no available trips that matched your search.'
+      else
+        return error_flash(@trips) if @trips.class == String
+        write_to_cache(@trips)
+        @hot_trips, @warm_trips, @cool_trips, @cold_trips = Trip.group_by_weather(@trips)
+      end
     elsif params[:commit] == 'Lucky Location'
       @trip = SearchFacade.get_lucky(flight_params)
       if @trip == []
