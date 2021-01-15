f65ba4048d0e6a064c73a5648778e0bf249a60f8
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-14 17:08:38 -0700

    Book flight button only viewable if logged in
    Co-authored-by: Jesse Mellinger <29828129+JesseMellinger@users.noreply.github.com>

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -112,4 +112,58 @@ describe 'when I click on a link to show a specific flight' do
     expect(page).to have_css('.TripAirbnb')
     expect(page).to have_css('.TripHotels')
   end
+
+  it "shows a message to login with google if I'm not logged in" do
+    json1 = File.read('spec/fixtures/trip_id_return.json')
+    json2 = File.read('spec/fixtures/trip_return.json')
+    query = '?departure_airport=DEN&departure_date=30/01/2021&limit=1&trip_duration=3&uid'
+    json1_parsed = JSON.parse(json1, symbolize_names: true)
+
+    if ENV['WEBMOCK'] == 'true'
+      stub_request(:get, "#{ENV['BACKEND_URL']}/api/v1/search#{query}")
+        .to_return(status: 200, body: json1, headers: {})
+    end
+
+    if ENV['WEBMOCK'] == 'true'
+      stub_request(:get, "#{ENV['BACKEND_URL']}/api/v1/trips/#{json1_parsed[:data][:trip_id]}")
+        .to_return(status: 200, body: json2, headers: {})
+    end
+
+    visit '/'
+
+    select 'Denver International', from: 'departure_airport'
+    fill_in 'departure_date', with: '2021-01-30'
+    fill_in 'trip_duration', with: 3
+    click_button('Lucky Location')
+
+    expect(page).to have_content("Please login with Google to book this flight!")
+  end
+
+  it "shows a message to book the flight if I'm logged in" do
+    json1 = File.read('spec/fixtures/trip_id_return.json')
+    json2 = File.read('spec/fixtures/trip_return.json')
+    query = '?departure_airport=DEN&departure_date=30/01/2021&limit=1&trip_duration=3&uid'
+    json1_parsed = JSON.parse(json1, symbolize_names: true)
+
+    if ENV['WEBMOCK'] == 'true'
+      stub_request(:get, "#{ENV['BACKEND_URL']}/api/v1/search#{query}")
+        .to_return(status: 200, body: json1, headers: {})
+    end
+
+    if ENV['WEBMOCK'] == 'true'
+      stub_request(:get, "#{ENV['BACKEND_URL']}/api/v1/trips/#{json1_parsed[:data][:trip_id]}")
+        .to_return(status: 200, body: json2, headers: {})
+    end
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(true)
+
+    visit '/'
+
+    select 'Denver International', from: 'departure_airport'
+    fill_in 'departure_date', with: '2021-01-30'
+    fill_in 'trip_duration', with: 3
+    click_button('Lucky Location')
+
+    expect(page).to have_button("Book Your Flight")
+  end
 end
